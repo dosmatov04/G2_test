672f611ff032acfa7136933715beef8ec6a1cfd0
dosmatov04 <dosmatovvd.04@gmail.com>
2021-09-20 19:37:38 +0600

    initital commit

---
+++ oop.py
@@ -0,0 +1,30 @@
+# class Cow:
+#     def make_sound(self):
+#         print("my-my")
+    
+# cow.make_sound("my-my")
+
+# class Cars:
+#     def __init__(self,brand,model,years,odometr,type_of,volume):
+#         self.brand = brand
+#         self.model = model
+#         self.years = years
+#         self.odometr = odometr
+#         self.type_of = type_of
+#         self.volume = volume
+#         self.is_going = True
+    
+#     def car_is_going(self,km):
+#         self.odometr += km
+#         self.is_going=True
+        
+#     def __str__(self):
+#         return f"{self.brand} --- {self.model} --- {self.years} --- {self.odometr}km/h --- {self.type_of} --- {self.volume} --- {self.is_going}"
+    
+# car = Cars("Toyota","camry",2021,0,"sport",3.5)
+# print(car)
+# car.car_is_going(102)
+# print(car)
+
+
+        
